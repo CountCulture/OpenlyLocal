bef32a87113071e50120aad24c486309efa50e01
CountCulture <countculture@googlemail.com>
2009-07-05 15:51:26 +0100

    Added link_to_calendar helper method

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,2 +1,5 @@
 module MeetingsHelper
+  def link_to_calendar
+    link_to "Subscribe to this calendar", url_for(params.merge(:protocol => "webcal", :only_path => false, :format => "ics")), :class => "calendar feed"
+  end
 end
