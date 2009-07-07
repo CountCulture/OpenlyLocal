9dd95ae4f2f683a69acb0a7f5d29f18244424d3a
CountCulture <countculture@googlemail.com>
2009-07-07 13:23:16 +0100

    Added sitemap plugin

---
+++ config/sitemap.rb
@@ -0,0 +1,44 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+
+SitemapGenerator::Sitemap.add_links do |sitemap|
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: sitemap.add path, options
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly', 
+  #           :lastmod => Time.now, :host => default_host
+
+  
+  # Examples:
+  
+  # add '/councils'
+  sitemap.add councils_path, :priority => 0.7, :changefreq => 'daily'
+
+  # add parsed councils
+  Council.parsed.each do |c|
+    sitemap.add council_path(c), :lastmod => c.updated_at
+  end
+
+  # add members
+  Member.all.each do |m|
+    sitemap.add member_path(m), :lastmod => m.updated_at
+  end
+
+  # add committees
+  Committee.all.each do |c|
+    sitemap.add committee_path(c), :lastmod => c.updated_at
+  end
+
+  # add meetins
+  Meeting.all.each do |m|
+    sitemap.add meeting_path(m), :lastmod => m.updated_at
+  end
+
+  sitemap.add '/info/about_us', :priority => 0.7
+  
+end
