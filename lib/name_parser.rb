694775a51c5f2ec4127082f16139a48b01b05bca
CountCulture <countculture@googlemail.com>
2009-08-30 17:53:16 +0100

    Forget to double-escape char classes in NameParser. Messing up some names

--- lib/name_parser.rb
+++ lib/name_parser.rb
@@ -9,9 +9,9 @@ module NameParser
     poss_quals = Qualifications + Qualifications.map{|e| e.gsub('.','')}
     titles, qualifications, result_hash = [], [], {}
     fn = fn.sub(/(Councillor|Councilllor|Cllr|Councillior|CC)\b/, '')
-    titles = Titles.select{ |t| fn.sub!(Regexp.new("#{t}.?\s"),'')}
+    titles = Titles.select{ |t| fn.sub!(Regexp.new("#{t}\\.?\\s"),'')}
     fn.strip! #sho initials should have no white space before them
-    qualifications = poss_quals.select{ |q| fn.sub!(Regexp.new("\s#{Regexp.escape(q)}"),'')}.compact
+    qualifications = poss_quals.select{ |q| fn.sub!(Regexp.new("\\s#{Regexp.escape(q)}"),'')}.compact
     names = fn.gsub(/([.,])/, ' ').gsub(/\([\w ]+\)/, '').gsub(/(\s[A-Z]{3,})+$/, '').split(" ")
 
     result_hash[:first_name] = names[0..-2].join(" ")
