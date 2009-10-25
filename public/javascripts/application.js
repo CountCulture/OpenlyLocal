602c51accc7130bed417794b22f5840b60ccb47f
CountCulture <countculture@googlemail.com>
2009-10-25 23:21:10 +0000

    First draft of jquery autocomplete search for council services

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,29 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// With help from http://blog.schuager.com/2008/09/jquery-autocomplete-json-apsnet-mvc.html and http://1300grams.com/2009/08/17/jquery-autocomplete-with-json-jsonp-support-and-overriding-the-default-search-parameter-q/
+$(document).ready( function() {
+  $('#term').autocomplete("/services.json", {
+      dataType: 'json',
+      parse: function(data) {
+          var rows = new Array();
+          for(var i=0; i<data.length; i++){
+              rows[i] = { data:data[i].service, value:data[i].service.title, result:data[i].service.title };
+          }
+          return rows;
+      },
+      formatItem: function(row, i, n) {
+          return '<a href="'+ row.url + '" class="external">' + row.title + '</a>';
+      },
+      width: 300,
+			extraParams: {
+				council_id: function() { return $("#council_id").val(); },
+				q: '',
+				minChars: 2,
+				term: function() { return $("#term").val();}
+			},
+      mustMatch: true
+  }).result(function(event, item) {
+			  location.href = item.url;
+			});;
+
+
+
+});
