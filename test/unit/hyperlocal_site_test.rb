3452edb64c1a93428fc55e20ca3f56a087e17a70
CountCulture <countculture@googlemail.com>
2009-12-23 15:57:00 +0000

    Missed file from earlier commit

---
+++ test/unit/hyperlocal_site_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class HyperlocalSiteTest < ActiveSupport::TestCase
+  subject { @hyperlocal_site }
+  
+  context "The HyperlocalSite class" do
+    setup do
+      @hyperlocal_site = Factory(:hyperlocal_site)
+    end
+    
+    should_validate_uniqueness_of :title
+    should_validate_presence_of :title
+    should_validate_presence_of :url
+    should_validate_uniqueness_of :url
+    
+    should_have_db_column :email
+    should_have_db_column :lat
+    should_have_db_column :lng
+    should_have_db_column :distance
+    should_have_db_column :twitter_account
+    should_have_db_column :feed_url
+        
+  end
+  
+  context "A HyperlocalSite instance" do
+    setup do
+      @hyperlocal_site = Factory(:hyperlocal_site)
+    end
+    
+  end
+  
+end
