2d31b06a896ca88e4bcef40d4b51972cc5bb22ee
CountCulture <countculture@googlemail.com>
2010-04-09 17:48:23 +0100

    Can now view services for all councils by services id

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,8 +1,13 @@
 class ServicesController < ApplicationController
   def index
-    @council = Council.find(params[:council_id])
-    @services = @council.services.matching_term(params[:term])
-    @title = params[:term] ? "Links to Services named '#{params[:term]}'" : "Links to Services"
+    if @council = Council.find_by_id(params[:council_id])
+      @services = @council.services.matching_term(params[:term])
+      @title = params[:term] ? "Links to Services named '#{params[:term]}'" : "Links to Services"
+    else
+      @ldg_service = LdgService.find(params[:ldg_service_id])
+      @services = @ldg_service.services
+      @title = "#{@ldg_service.title} : #{@ldg_service.category}"
+    end
     respond_to do |format|
       format.html do
         @services = @services.group_by(&:category).sort_by{ |c| c[0] }
--- test/functional/services_controller_test.rb
+++ test/functional/services_controller_test.rb
@@ -4,139 +4,168 @@ class ServicesControllerTest < ActionController::TestCase
   def setup
     @council = Factory(:council, :authority_type => "district", :ldg_id => 42)
     @service = Factory(:service, :council => @council)
+    @ldg_service = @service.ldg_service
     @another_service = Factory(:service, :category => "Bar category", :title => "Bar service", :council => @council)
   end
 
   # index test
-   context "on GET to :index" do
-     
-     context "with basic request and council" do
-       setup do
-         get :index, :council_id => @council.id
-       end
-       
-       should_assign_to :services
-       should_assign_to :council
-       should_respond_with :success
-       should_render_template :index
+  context "on GET to :index" do
+    
+    context "with basic request and council" do
+      setup do
+        get :index, :council_id => @council.id
+      end
       
-       should "show services in title" do
-         assert_select "title", /links to services/i
-       end
-       
-       should "show council in title" do
-         assert_select "title", /#{@council.title}/
-       end
-       
-       should "list links to services" do
-         assert_select "div#services li a", @service.title do
-           assert_select "a[href='#{ERB::Util::h @service.url}']" #uris are escaped by default
-         end
-       end
+      should_assign_to :services
+      should_assign_to :council
+      should_respond_with :success
+      should_render_template :index
      
-       should "show list of categories" do
-         assert_select "div#categories ul li", 2  do # two different categories
-           assert_select "a", "Bar category"
-         end
-       end
-       
-       should "group links by category" do
-         assert_select "div#services h3", 2  do # two different categories
-           assert_select "h3", "Bar category"
-         end
-       end
-       
-       should "put include search form" do
-         assert_select "form#services_search"
-       end
-       
-       should "include hidden field for council id in search form" do
-         assert_select "form#services_search input#council_id[type='hidden'][value='#{@council.id}']"
-       end
-     end
-     
-     context "with basic request and xml requested" do
-       setup do
-         get :index, :council_id => @council.id, :format => "xml"
-       end
-       
-       should_assign_to :services
-       should_assign_to :council
-       should_respond_with :success
-       should_render_without_layout
-       should_respond_with_content_type 'application/xml'
-       
-       should "show services for council" do
-         assert_select "services>service>title", @service.title
-         assert_select "services>service>url", @service.url
-       end
-
-     end
-     
-     context "with basic request and json requested" do
-       setup do
-         get :index, :council_id => @council.id, :format => "json"
-       end
-       
-       should_assign_to :services
-       should_assign_to :council
-       should_respond_with :success
-       should_render_without_layout
-       should_respond_with_content_type 'application/json'
-       
-       should "show services for council" do
-         assert_match /service.+title.+#{@service.title}/, @response.body
-         assert_match /service.+url.+#{@service.url}/, @response.body
-       end
-     end
-     
-     context "with basic request and search term" do
-       setup do
-         get :index, :council_id => @council.id, :term => "foo"
-       end
-       
-       should_assign_to(:services)
-       should_assign_to :council
-       should_respond_with :success
-       should_render_template :index
+      should "show services in title" do
+        assert_select "title", /links to services/i
+      end
       
-       should "show term in title" do
-         assert_select "title", /foo/i
-       end
-       
-       should "show only titles with foo in title" do
-         assert_select "div#services li a", @service.title
-         assert_select "div#services li a", :text => @another_service.title, :count => 0
-       end
-       
-       should "put term in form input box" do
-         assert_select "form#services_search" do
-           assert_select "input[value='foo']"
-         end
-       end
-       
-     end
+      should "show council in title" do
+        assert_select "title", /#{@council.title}/
+      end
+      
+      should "list links to services" do
+        assert_select "div#services li a", @service.title do
+          assert_select "a[href='#{ERB::Util::h @service.url}']" #uris are escaped by default
+        end
+      end
+    
+      should "show list of categories" do
+        assert_select "div#categories ul li", 2  do # two different categories
+          assert_select "a", "Bar category"
+        end
+      end
+      
+      should "group links by category" do
+        assert_select "div#services h3", 2  do # two different categories
+          assert_select "h3", "Bar category"
+        end
+      end
+      
+      should "put include search form" do
+        assert_select "form#services_search"
+      end
+      
+      should "include hidden field for council id in search form" do
+        assert_select "form#services_search input#council_id[type='hidden'][value='#{@council.id}']"
+      end
+    end
+    
+    context "with basic request and ldg_service" do
+      setup do
+        get :index, :ldg_service_id => @service.ldg_service_id
+      end
+      
+      should_assign_to :ldg_service
+      should_assign_to :services
+      should_respond_with :success
+      should_render_template :index
+      
+      should "show ldg_service in title" do
+        assert_select "title", /#{@ldg_service.title}/
+      end
+      
+      should "list councils in links" do
+        assert_select "div#services li a", @council.title
+      end
+      
+      should "list links to services" do
+        assert_select "div#services li a", @council.title do
+          assert_select "a[href='#{ERB::Util::h @service.url}']" #uris are escaped by default
+        end
+      end
+    
+    end
+    
+    context "with basic request and xml requested" do
+      setup do
+        get :index, :council_id => @council.id, :format => "xml"
+      end
+      
+      should_assign_to :services
+      should_assign_to :council
+      should_respond_with :success
+      should_render_without_layout
+      should_respond_with_content_type 'application/xml'
+      
+      should "show services for council" do
+        assert_select "services>service>title", @service.title
+        assert_select "services>service>url", @service.url
+      end
+  
+    end
+    
+    context "with basic request and json requested" do
+      setup do
+        get :index, :council_id => @council.id, :format => "json"
+      end
+      
+      should_assign_to :services
+      should_assign_to :council
+      should_respond_with :success
+      should_render_without_layout
+      should_respond_with_content_type 'application/json'
+      
+      should "show services for council" do
+        assert_match /service.+title.+#{@service.title}/, @response.body
+        assert_match /service.+url.+#{@service.url}/, @response.body
+      end
+    end
+    
+    context "with basic request and search term" do
+      setup do
+        get :index, :council_id => @council.id, :term => "foo"
+      end
+      
+      should_assign_to(:services)
+      should_assign_to :council
+      should_respond_with :success
+      should_render_template :index
      
-     context "when no services" do
-       setup do
-         Service.delete_all
-         get :index, :council_id => @council.id
-       end
-       should_assign_to :council
-       should_respond_with :success
-       should "show message" do
-         assert_select "p", /no services found/i
-       end
-       should "not show services block" do
-         assert_select "div#services", false
-       end
-     end
-     
-     context "on GET to :index without council_id" do
+      should "show term in title" do
+        assert_select "title", /foo/i
+      end
+      
+      should "show only titles with foo in title" do
+        assert_select "div#services li a", @service.title
+        assert_select "div#services li a", :text => @another_service.title, :count => 0
+      end
+      
+      should "put term in form input box" do
+        assert_select "form#services_search" do
+          assert_select "input[value='foo']"
+        end
+      end
+      
+    end
+    
+    context "when no services" do
+      setup do
+        Service.delete_all
+        get :index, :council_id => @council.id
+      end
+      should_assign_to :council
+      should_respond_with :success
+      should "show message" do
+        assert_select "p", /no services found/i
+      end
+      should "not show services block" do
+        assert_select "div#services", false
+      end
+    end
+    
+    context "on GET to :index without council_id" do
+  
+      should "raise an exception" do
+        assert_raise(ActiveRecord::RecordNotFound) { get :index }
+      end
+    end
+  end
+  
 
-       should "raise an exception" do
-         assert_raise(ActiveRecord::RecordNotFound) { get :index }
-       end
-     end
-   end
 end
