01451d08ecb4271dd61d5397a08b909f2486625a
CountCulture <countculture@googlemail.com>
2010-07-26 20:01:38 +0100

    Add more models to RelatedArticle::ModelsAcceptingPingbacks

--- app/models/related_article.rb
+++ app/models/related_article.rb
@@ -5,13 +5,13 @@ class RelatedArticle < ActiveRecord::Base
   validates_uniqueness_of :url
   default_scope :order => 'created_at'
   delegate :openlylocal_url, :to => :subject, :allow_nil => true
-  ModelsAcceptingPinbacks = %w(members committees meetings polls)
+  ModelsAcceptingPingbacks = %w(members committees meetings polls financial_transactions suppliers companies)
   
   def self.process_pingback(pingback)
     subject_params = pingback.target_uri.scan(/openlylocal.com\/([\w_]+)\/(\d+)/i).flatten
     hyperlocal_site = HyperlocalSite.find_from_article_url(pingback.source_uri)
     logger.error { "****Pingback received: #{pingback.inspect}" }
-    return false unless subject_params.size == 2 && ModelsAcceptingPinbacks.include?(subject_params.first)
+    return false unless subject_params.size == 2 && ModelsAcceptingPingbacks.include?(subject_params.first)
     subject = subject_params.first.classify.constantize.find_by_id(subject_params[1]) rescue nil
     if subject && hyperlocal_site
       create( :title => pingback.title,
