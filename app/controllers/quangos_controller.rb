602571f36d6ff925cbb9e0c4747dd01cfd3de7f0
CountCulture <countculture@googlemail.com>
2010-09-21 13:18:23 +0100

    Added Quango index page

--- app/controllers/quangos_controller.rb
+++ app/controllers/quangos_controller.rb
@@ -1,4 +1,15 @@
 class QuangosController < ApplicationController
+
+  def index
+    @quangos = Quango.all
+    @title = 'Quangos and other organisations'
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @quangos.to_xml }
+      format.json { render :json => @quangos.to_json }
+    end
+  end
+  
   def show
     @quango = Quango.find(params[:id])
     @title = "#{@quango.title} :: Quangos"
--- test/functional/quangos_controller_test.rb
+++ test/functional/quangos_controller_test.rb
@@ -5,6 +5,56 @@ class QuangosControllerTest < ActionController::TestCase
     @quango = Factory(:quango)
   end
   
+  context "on GET to :index" do
+    
+    context "with basic request" do
+      setup do
+        get :index
+      end
+
+      should_assign_to(:quangos) { Quango.find(:all)}
+      should respond_with :success
+      should render_template :index
+      should "list quangos" do
+        assert_select "li a", @quango.title
+      end
+      
+      should "show share block" do
+        assert_select "#share_block"
+      end
+
+      should "show api block" do
+        assert_select "#api_info"
+      end
+      
+      should 'show title' do
+        assert_select "title", /quangos/i
+      end
+    end
+    
+    context "with xml request" do
+      setup do
+        get :index, :format => "xml"
+      end
+
+      should_assign_to(:quangos) { Quango.find(:all) }
+      should respond_with :success
+      should_render_without_layout
+      should respond_with_content_type 'application/xml'
+    end
+
+    context "with json requested" do
+      setup do
+        get :index, :format => "json"
+      end
+
+      should_assign_to(:quangos) { Quango.find(:all) }
+      should respond_with :success
+      should_render_without_layout
+      should respond_with_content_type 'application/json'
+    end
+  end
+  
   context "on GET to :show" do
     context "in general" do
       setup do
