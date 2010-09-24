64a41503158034e2ec01cc79380221d220059dba
CountCulture <countculture@googlemail.com>
2010-09-24 09:13:21 +0100

    Replace very costly count of unprocessed delayed_jobs with rough direct SQL one

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,6 +6,7 @@ class AdminController < ApplicationController
     @hyperlocal_sites = HyperlocalSite.find_all_by_approved(false)
     @user_submissions = UserSubmission.unapproved
     @unapproved_contacts = CouncilContact.unapproved
+    @delayed_job_count = Delayed::Job.connection.execute("show table status like 'delayed_jobs'").fetch_hash["Rows"]
   end
 
 end
