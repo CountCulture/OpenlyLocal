65991010d4c6b4acfd9f2063ece5c51005661c15
CountCulture <countculture@googlemail.com>
2010-10-25 09:32:48 +0100

    Add DB indices for classification_links and charity_annual_reports

---
+++ db/migrate/20101025082944_add_missing_indices.rb
@@ -0,0 +1,13 @@
+class AddMissingIndices < ActiveRecord::Migration
+  def self.up
+    add_index :classification_links, [:classified_id, :classified_type]
+    add_index :classification_links, :classification_id
+    add_index :charity_annual_reports, :charity_id
+  end
+
+  def self.down
+    remove_index :classification_links, :classification_id
+    remove_index :classification_links, [:classified_id, :classified_type]
+    remove_index :charity_annual_reports, :charity_id
+  end
+end
