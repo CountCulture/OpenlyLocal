e2bd8904de2b1dfec90a651673824d169f30817b
CountCulture <countculture@googlemail.com>
2011-01-19 17:56:21 +0000

    Optimised SQL calls w/eager loading on admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,8 +4,8 @@ class AdminController < ApplicationController
   def index
     @title = 'Admin'
     @hyperlocal_sites = HyperlocalSite.find_all_by_approved(false)
-    @user_submissions = UserSubmission.unapproved(:include=>:item)
-    @unapproved_contacts = CouncilContact.unapproved
+    @user_submissions = UserSubmission.unapproved.all(:include=>:item)
+    @unapproved_contacts = CouncilContact.unapproved.all(:include => :council)
     @delayed_job_count = Delayed::Job.connection.execute("show table status like 'delayed_jobs'").fetch_hash["Rows"]
     @unimported_spending_data_councils = Service.spending_data_services_for_councils
     @senior_staff_data_councils = Service.for_lgsl_id(LdgService::SENIOR_STAFF_LGSL)
