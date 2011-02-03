277457eb389332ebf12b48cc15c3bc460e8851bb
CountCulture <countculture@googlemail.com>
2011-02-03 09:15:09 +0000

    Update mailers to admin@openlylocal.com

--- app/models/scraper_mailer.rb
+++ app/models/scraper_mailer.rb
@@ -4,7 +4,7 @@ class ScraperMailer < ActionMailer::Base
   def auto_scraping_report(report_hash)
     subject    "OpenlyLocal :: Auto Scraping Report :: #{report_hash[:summary]}"
     recipients 'countculture@googlemail.com'
-    from       'countculture@googlemail.com'
+    from       'admin@openlylocal.com'
     sent_on    Time.now
     
     body       :report => report_hash[:report]
@@ -13,7 +13,7 @@ class ScraperMailer < ActionMailer::Base
   def scraping_report(scraper)
     subject    "Scraping Report :: #{scraper.title} :: #{scraper.results_summary}"
     recipients 'countculture@googlemail.com'
-    from       'countculture@googlemail.com'
+    from       'admin@openlylocal.com'
     sent_on    Time.now
     content_type "text/html"
     
