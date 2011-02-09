625628ad7cb997944ccc0a411dd44bd7fc8bb7d2
CountCulture <countculture@googlemail.com>
2011-02-09 22:09:55 +0000

    Tweak to storing of FinancialTransaction in delayed job, and correct error in caching

--- app/models/observers/financial_transaction_observer.rb
+++ app/models/observers/financial_transaction_observer.rb
@@ -3,8 +3,9 @@ class FinancialTransactionObserver < ActiveRecord::Observer
     unless ft.supplier.vat_number.blank?  || ft.supplier.payee || ft.supplier.failed_payee_search
       Delayed::Job.enqueue(SupplierUtilities::VatMatcher.new(:vat_number => ft.supplier.vat_number, :supplier => ft.supplier, :title => ft.supplier.title)) 
     end
-    ft.reload #reload to avoid serialisation probs
-    Delayed::Job.enqueue(ft) # add financail_transaction to DelayedJob, after VatMatcher
+    # ft.clear_association_cache #reload to avoid serialisation probs
+    Rails.logger.info { "About to add financial_transaction to DelayedJob queue:\n#{ft.to_yaml}" }
+    Delayed::Job.enqueue(FinancialTransaction.find(ft.id))# reload FinancialTransaction (nb ft.clear_association_cache or ft.rleoad should work but soesn't seem to), add financial_transaction to DelayedJob, after VatMatcher
     true
   end
     
