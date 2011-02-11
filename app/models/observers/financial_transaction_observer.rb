6ecc614f2f733eb0b090f7e6bda5795f180e0d20
CountCulture <countculture@googlemail.com>
2011-02-11 09:30:39 +0000

    Bugfix: was logging financial_transaction with all associations. Massively slowing down import and casuing memory issues

--- app/models/observers/financial_transaction_observer.rb
+++ app/models/observers/financial_transaction_observer.rb
@@ -4,7 +4,7 @@ class FinancialTransactionObserver < ActiveRecord::Observer
       Delayed::Job.enqueue(SupplierUtilities::VatMatcher.new(:vat_number => ft.supplier.vat_number, :supplier => ft.supplier, :title => ft.supplier.title)) 
     end
     # ft.clear_association_cache #reload to avoid serialisation probs
-    Rails.logger.info { "About to add financial_transaction to DelayedJob queue:\n#{ft.to_yaml}" }
+    # Rails.logger.info { "About to add financial_transaction to DelayedJob queue:\n#{ft.to_yaml}" }
     Delayed::Job.enqueue(FinancialTransaction.find(ft.id))# reload FinancialTransaction (nb ft.clear_association_cache or ft.rleoad should work but soesn't seem to), add financial_transaction to DelayedJob, after VatMatcher
     true
   end
