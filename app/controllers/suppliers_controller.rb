7147acc4971235e25772d708c9d9868d482b6637
CountCulture <countculture@googlemail.com>
2011-03-14 21:26:28 +0000

    Reduce supplier pagination limit

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -4,7 +4,7 @@ class SuppliersController < ApplicationController
   def index
     search_filter = params[:name_filter]
     page = (params[:page]||1).to_i
-    page = 500 if page > 500
+    page = 50 if page > 50
     @organisation = params[:organisation_type].constantize.find(params[:organisation_id]) if params[:organisation_type] && params[:organisation_id]
     @title = @organisation ? "Suppliers to #{@organisation.title}" : 'Suppliers to Local Authorities'
     sort_order = params[:order] == 'total_spend' ? 'spending_stats.total_spend DESC' : 'name'
