b30c25420281ee07ea8dad087acde824df2477f2
CountCulture <countculture@googlemail.com>
2011-11-06 14:30:51 +0000

    Run scrapers more frequently

--- config/schedule.rb
+++ config/schedule.rb
@@ -16,7 +16,7 @@ end
   # command "cd ~/sites/twfy_local/current && RAILS_ENV=production LIMIT=2 EMAIL_RESULTS=true /opt/ruby-enterprise-1.8.6/bin/rake run_stale_scrapers >> log/cron_log 2>&1"
 # end
 
-every 3.hours do
+every 1.hours do
   command "/opt/ruby-enterprise-1.8/bin/ruby /home/cculture/sites/twfy_local/current/script/runner -e production 'Scraper.unproblematic.stale.find(:all, :limit => 30).each{|scraper| Delayed::Job.enqueue scraper} if Delayed::Job.count == 0' >> /home/cculture/sites/twfy_local/current/log/cron_log.log 2>&1"
 end
 every 2.hours, :at => 15 do
