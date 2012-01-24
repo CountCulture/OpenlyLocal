ac55a302840294be6ff1b4cb2329ee28d253e965
CountCulture <countculture@googlemail.com>
2012-01-24 09:40:25 +0000

    Add charity_worker to handle delayed_jobs for Charities (as OpenlyLocal uses different version of DJ than OpenCharities)

---
+++ test/unit/charity_worker_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class CharityWorkerTest < ActiveSupport::TestCase
+  
+  context "CharityWorker" do
+    setup do
+      @charity = Factory(:charity)
+      @charity_worker = CharityWorker.new(@charity, :foo_method)
+    end
+    
+    # teardown do
+    #   Charity.destroy_all
+    # end
+    # 
+    context "on instantiation" do
+
+      should "should store charity id as charity_id" do
+        assert_equal @charity.id, @charity_worker.charity_id
+      end
+
+      should "should store given method as delayed_meth" do
+        assert_equal :foo_method, @charity_worker.delayed_meth
+      end
+    end
+
+    context 'when asked to perform' do
+
+      should 'run delayed_meth on charity identified by charity_id' do
+        Charity.expects(:find).with(@charity.id).returns(@charity)
+        @charity.expects(:foo_method)
+        @charity_worker.perform
+      end
+
+    end
+
+  end
+
+end
