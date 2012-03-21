058794af99fea2d96ce6ef62d64b25e0273fbbf4
CountCulture <countculture@googlemail.com>
2012-03-21 12:24:45 +0000

    tweak javascript obfuscation

--- app/helpers/planning_applications_helper.rb
+++ app/helpers/planning_applications_helper.rb
@@ -1,5 +1,6 @@
 module PlanningApplicationsHelper
   def javascript_obfuscate(text)
+    return if text.blank?
     string = ''
     "document.write('#{h(text)}');".each_byte do |c|
       string << sprintf("%%%x", c)
