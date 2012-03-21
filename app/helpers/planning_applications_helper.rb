a6a41f2685e21153dd02de3b7926273d250f00f9
CountCulture <countculture@googlemail.com>
2012-03-21 12:19:34 +0000

    javascript obfuscate applicant name for planning applications

--- app/helpers/planning_applications_helper.rb
+++ app/helpers/planning_applications_helper.rb
@@ -1,2 +1,9 @@
 module PlanningApplicationsHelper
+  def javascript_obfuscate(text)
+    string = ''
+    "document.write('#{h(text)}');".each_byte do |c|
+      string << sprintf("%%%x", c)
+     end
+     "<script type=\"#{Mime::JS}\">eval(decodeURIComponent('#{string}'))</script>"
+  end
 end
