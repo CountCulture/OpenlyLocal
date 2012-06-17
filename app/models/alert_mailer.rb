4d113b2d7d7df8f498e36cf7c0c08c6b249350ba
James McKinney <james@slashpoundbang.com>
2012-06-17 12:12:55 -0400

    add name to from header

--- app/models/alert_mailer.rb
+++ app/models/alert_mailer.rb
@@ -4,7 +4,7 @@ class AlertMailer < ActionMailer::Base
 
   def confirmation(subscriber)
     @recipients   = subscriber.email
-    @from         = 'alerts@openlylocal.com'
+    @from         = 'OpenlyLocal Alerts <alerts@openlylocal.com>'
     @subject      = "Please confirm your OpenlyLocal Planning Alerts subscription"
     @sent_on      = Time.now
     @body[:subscriber] = subscriber
@@ -13,7 +13,7 @@ class AlertMailer < ActionMailer::Base
 
   def unsubscribe_confirmation(subscriber)
     @recipients   = subscriber.email
-    @from         = 'alerts@openlylocal.com'
+    @from         = 'OpenlyLocal Alerts <alerts@openlylocal.com>'
     @subject      = "You've been unsubscribed from OpenlyLocal Planning Alerts"
     @sent_on      = Time.now
     @body[:subscriber] = subscriber
@@ -22,8 +22,8 @@ class AlertMailer < ActionMailer::Base
   
   def planning_alert(params={})
     @recipients   = params[:subscriber].email
-    @from         = 'alerts@openlylocal.com'
-    @subject      = "OpenlyLocal Alert :: New Planning Application: #{params[:planning_application].address}"
+    @from         = 'OpenlyLocal Alerts <alerts@openlylocal.com>'
+    @subject      = "New Planning Application: #{params[:planning_application].address}"
     @sent_on      = Time.now
     @body[:planning_application] = params[:planning_application]
     @body[:subscriber] = params[:subscriber]
