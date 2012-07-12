02d879f5fb2d171aa7727a48ba08d38ecd9efcb7
James McKinney <james@slashpoundbang.com>
2012-07-12 12:42:12 -0400

    fix HELO domain (in production, too) and simplify smtp_gmail.rb

--- config/initializers/smtp_gmail.rb
+++ config/initializers/smtp_gmail.rb
@@ -6,9 +6,4 @@ config_file = "#{RAILS_ROOT}/config/smtp_gmail.yml"
 raise "Sorry, you must have #{config_file}" unless File.exists?(config_file)
 
 config_options = YAML.load_file(config_file)
-ActionMailer::Base.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}.merge(config_options['default'].symbolize_keys ) # Configuration options override default options
+ActionMailer::Base.smtp_settings = config_options['default'].symbolize_keys
