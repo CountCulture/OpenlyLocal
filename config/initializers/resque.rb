a0e9854304f1ba3be762c86217d8a58d627ba0cb
James McKinney <james@slashpoundbang.com>
2012-07-12 16:29:05 -0400

    try multiple redis failure backends

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,10 +1,16 @@
 require 'resque/server'
-require 'resque/failure/airbrake'
 
+# Consider using: https://github.com/lantins/resque-retry
+# https://github.com/defunkt/resque/wiki/Failure-Backends
+require 'resque/failure/multiple'
+require 'resque/failure/airbrake'
+require 'resque/failure/redis'
 Resque::Failure::Airbrake.configure do |config|
   config.api_key = '3f276ee6addc559ee3f05304075c7798'
   config.secure = true
 end
+Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Airbrake]
+Resque::Failure.backend = Resque::Failure::Multiple
 
 rails_env = ENV['RAILS_ENV'] || 'development'
 
