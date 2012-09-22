3556093db39ebb61f892d4706ab6c9e71eabe45b
James McKinney <james@slashpoundbang.com>
2012-09-22 19:25:43 -0400

    speed up migration

--- db/migrate/20120922160147_remove_limits_from_planning_applications.rb
+++ db/migrate/20120922160147_remove_limits_from_planning_applications.rb
@@ -1,13 +1,23 @@
 class RemoveLimitsFromPlanningApplications < ActiveRecord::Migration
   def self.up
-    change_column :planning_applications, :postcode, :string, :limit => nil
-    change_column :planning_applications, :decision, :string, :limit => nil
-    change_column :planning_applications, :application_type, :string, :limit => nil
+    # These take forever:
+    #
+    #change_column :planning_applications, :postcode, :string, :limit => nil
+    #change_column :planning_applications, :decision, :string, :limit => nil
+    #change_column :planning_applications, :application_type, :string, :limit => nil
+    PlanningApplication.connection.execute "UPDATE pg_attribute SET atttypmod = 259 WHERE attrelid = 'planning_applications'::regclass AND attname = 'postcode';"
+    PlanningApplication.connection.execute "UPDATE pg_attribute SET atttypmod = 259 WHERE attrelid = 'planning_applications'::regclass AND attname = 'decision';"
+    PlanningApplication.connection.execute "UPDATE pg_attribute SET atttypmod = 259 WHERE attrelid = 'planning_applications'::regclass AND attname = 'application_type';"
   end
 
   def self.down
-    change_column :planning_applications, :postcode, :string, :limit => 10
-    change_column :planning_applications, :decision, :string, :limit => 64
-    change_column :planning_applications, :application_type, :string, :limit => 64
+    # These take forever:
+    #
+    #change_column :planning_applications, :postcode, :string, :limit => 10
+    #change_column :planning_applications, :decision, :string, :limit => 64
+    #change_column :planning_applications, :application_type, :string, :limit => 64
+    PlanningApplication.connection.execute "UPDATE pg_attribute SET atttypmod = 14 WHERE attrelid = 'planning_applications'::regclass AND attname = 'postcode';"
+    PlanningApplication.connection.execute "UPDATE pg_attribute SET atttypmod = 68 WHERE attrelid = 'planning_applications'::regclass AND attname = 'decision';"
+    PlanningApplication.connection.execute "UPDATE pg_attribute SET atttypmod = 68 WHERE attrelid = 'planning_applications'::regclass AND attname = 'application_type';"
   end
 end
