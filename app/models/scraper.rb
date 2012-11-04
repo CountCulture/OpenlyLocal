2c2a0e37c388bcc13b9b9ce62844ff239452a0f9
CountCulture <countculture@googlemail.com>
2012-11-04 23:54:34 +0000

    Add lock timeout

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -29,6 +29,7 @@ class Scraper < ActiveRecord::Base
   delegate :related_model, :to => :parser, :allow_nil => true
   delegate :portal_system, :to => :council, :allow_nil => true
   @queue = :scrapers
+  @lock_timeout = 600
   
   def validate
     errors.add(:parser, "can't be blank") unless parser
